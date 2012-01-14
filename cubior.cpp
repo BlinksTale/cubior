07d90ce3df6d9be45bd1e0c292af66ea705743df
Brian Handy <BlinksTale@gmail.com>
2012-01-13 17:12:53 -0800

    Initial files added, testing cpp

---
+++ cubior.cpp
@@ -0,0 +1,13 @@
+/*
+ * Cubior
+ * by Brian Handy
+ * 1/13/12
+ * Main class for 3d platformer
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Cubior here!\n";
+  return 0;
+}
