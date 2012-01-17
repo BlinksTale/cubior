e7beadf7b73fe76f141d350fe4b09a75e4f0db73
Brian Handy <BlinksTale@gmail.com>
2012-01-16 16:55:14 -0800

    Windows environment setup and documentation folder added

---
+++ src/cubior.cpp
@@ -0,0 +1,13 @@
+/*
+ * Cubior
+ * by Brian Handy
+ * 1/13/12
+ * Main class for 3d platformer
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Cubior here, now for Ubuntu AND Windows.\n";
+  return 0;
+}
