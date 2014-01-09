12f6745f9d1aa91fe3bdd5858da9db37a3e5f839
Brian Handy <BlinksTale@gmail.com>
2014-01-08 22:33:05 -0800

    Trying 1 update/sec

--- src/networking.cpp
+++ src/networking.cpp
@@ -43,7 +43,7 @@ const int localPlayerMax = 4;
 // Tools
 time_t startTime = time(0);
 int totalMilliseconds = 0;
-const int timesPerSec = 15;
+const int timesPerSec = 1;
 int millisecondModulo = (int)(1000.0/timesPerSec);
 
 // Game instance variables
