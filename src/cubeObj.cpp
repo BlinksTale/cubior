f91be8d349a8b8ed3ee3ffa5e7587648a274f0b6
Brian Handy <BlinksTale@gmail.com>
2014-03-26 23:46:13 -0700

    More chances to execute setPosAverage code

--- src/cubeObj.cpp
+++ src/cubeObj.cpp
@@ -619,7 +619,7 @@ void CubeObj::setZ(int n) { z = n; }
 void CubeObj::setPos(int n, int o, int p) { x = n, y = o, z = p; }
 void CubeObj::setPosAverage(int n, int o, int p) {
   // apply if not touching a player
-  if (justHit == NULL) {
+  if (justHit == NULL || !justHit->isPlayer()) {
     float bias = posAverageBias; // bias towards current position
     x = n*(1.0f-bias)+x*bias;
     y = o*(1.0f-bias)+y*bias;
